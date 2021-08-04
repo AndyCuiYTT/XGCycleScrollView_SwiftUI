a16194db72d892e02ed7680fcc963b47dacd52ed
CuiXg <AndyCuiYTT@163.com>
2021-08-04 15:18:55 +0800

    Initial Commit

---
+++ XGCycleScrollView_SwiftUI/XGCycleScrollView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XGCycleScrollView_SwiftUIApp.swift
+//  XGCycleScrollView_SwiftUI
+//
+//  Created by CuiXg on 2021/8/4.
+//
+
+import SwiftUI
+
+@main
+struct XGCycleScrollView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
